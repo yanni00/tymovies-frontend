e506bfae5cad39f816f77b44a130245be225ee27
YANNI <yanniflambas@hotmail.com>
2020-02-05 13:58:55 +1100

    Updates

---
+++ precache-manifest.6d6fd08b235240c8b240b1145ea13578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7870d67f736950f30cd57c6255521196",
+    "url": "/yanni00/tymovies-frontend.git/index.html"
+  },
+  {
+    "revision": "c005f3a2aa1fe828274d",
+    "url": "/yanni00/tymovies-frontend.git/static/css/main.b3d91107.chunk.css"
+  },
+  {
+    "revision": "08ebcef6710c3fafe085",
+    "url": "/yanni00/tymovies-frontend.git/static/js/2.37305981.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/yanni00/tymovies-frontend.git/static/js/2.37305981.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c005f3a2aa1fe828274d",
+    "url": "/yanni00/tymovies-frontend.git/static/js/main.39acad5c.chunk.js"
+  },
+  {
+    "revision": "7c789f1310b0c00d9819",
+    "url": "/yanni00/tymovies-frontend.git/static/js/runtime-main.2475bf19.js"
+  }
+]);
