6ed9eef429caaeafdfbfb4e6ca36a664f14058b1
Tony Guan <tonyg06@msn.com>
2020-02-06 17:05:25 +1100

    update

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,16 +2,19 @@
   import axios from 'axios';
   import styles from './Review.module.css';
 
+  const URL = 'http://localhost:3000/';
+
+
   class Review extends React.Component {
 
     state ={
-      reviews: '',
-
+      reviews: [],
+      comments: ''
     };
 
     componentDidMount() {
-      const URL = 'http://localhost:3000/reviews';
-      axios.get(URL)
+
+      axios.get(URL + "reviews")
       .then( res => {
         console.log('response:', res.data);
         this.setState({reviews: res.data})
@@ -21,10 +24,40 @@
       });
     }
 
+    handleInput = (event) => {
+      this.setState({comment: event.target.value});
+    }
+
+    handleSubmit = (event) => {
+      event.preventDefault()
+      console.log('event', event.target.id);
+      // console.log('review_id', review_id);
+
+      // axios.post(URL + "comments")
+    }
+
     render(){
       return(
         <div>
-          <h2>reviews here for movie ID : {this.props.movieId}</h2>
+          <p>Reviews here for movie ID: {this.props.movieId}</p>
+          {this.state.reviews.map( review => (
+            <div key={review.id}>
+              <h2>{review.name}</h2>
+              <p>{review.userReview}</p>
+              <h3>{review.user.name}</h3>
+
+              <form
+                id={review.id} className={styles.form} onSubmit={this.handleSubmit}>
+                  <textarea rows="12" cols="55" type="text" onChange={this.handleInput}/>
+                  <br/>
+                  <input className={styles.inputButton} type="submit" value="Comment" />
+                </form>
+            </div>
+          ))
+        }
+
+
+
         </div>
       );
     }
