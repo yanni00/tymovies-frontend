3af99c639cf6d904dfb2d25e188266749c5af398
YANNI <yanniflambas@hotmail.com>
2020-02-09 20:59:54 +1100

    changes to frontend usersignup

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yanni00/tymovies-frontend.git/precache-manifest.6d6fd08b235240c8b240b1145ea13578.js"
+  "/tymovies-frontend/precache-manifest.92d93b1c5bc48de411e9915add6e09ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yanni00/tymovies-frontend.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tymovies-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
