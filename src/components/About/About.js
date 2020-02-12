4c7ea9864956834398cb33a4a74ee2744cdd50a4
Tony Guan <tonyg06@msn.com>
2020-02-12 11:27:09 +1100

    updated about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,7 +14,22 @@
     render(){
       return(
         <div>
+          <p className={styles.text}>
+            This is a full stack group project with a team of two who are Tony and Yanni with a set amount of time frame about a week. <br/><br/>
+            This website uses The Movie Database API. <br/><br/>
+            It has similar concept as IMDB.<br/><br/>
+            Any users are allowed to search for movies. <br/><br/>
+            Users can sign up for accounts.<br/><br/>
+            Users with accounts can give reviews to any movies as well as comment on other users' reviews. <br/><br/>
+            Technologies used:
+            <ul>
+              <li>React</li>
+              <li>Ruby on Rails</li>
+              <li>CSS modules</li>
 
+            </ul>
+
+          </p>
         </div>
       );
     }
