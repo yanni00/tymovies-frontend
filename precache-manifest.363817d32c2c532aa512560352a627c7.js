2ef88a573e38f3fd28d2f05cd2231d8d8541aacf
YANNI <yanniflambas@hotmail.com>
2020-02-12 17:43:04 +1100

    Updates

--- precache-manifest.363817d32c2c532aa512560352a627c7.js
+++ precache-manifest.363817d32c2c532aa512560352a627c7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "969adb26b4473d21cfd557b208dfe572",
+    "revision": "84061979bbae5d63f3016e201d876fce",
     "url": "/tymovies-frontend/index.html"
   },
   {
-    "revision": "ee1be2596dd64757ea81",
+    "revision": "a79b984599210af63827",
     "url": "/tymovies-frontend/static/css/main.20369fdb.chunk.css"
   },
   {
-    "revision": "7f743b262eb946ab751f",
-    "url": "/tymovies-frontend/static/js/2.3684a5d9.chunk.js"
+    "revision": "0de6fdc9f77d3de36fb1",
+    "url": "/tymovies-frontend/static/js/2.9caa5bce.chunk.js"
   },
   {
     "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/tymovies-frontend/static/js/2.3684a5d9.chunk.js.LICENSE.txt"
+    "url": "/tymovies-frontend/static/js/2.9caa5bce.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ee1be2596dd64757ea81",
-    "url": "/tymovies-frontend/static/js/main.b7e1a9d0.chunk.js"
+    "revision": "a79b984599210af63827",
+    "url": "/tymovies-frontend/static/js/main.5adf60fa.chunk.js"
   },
   {
     "revision": "eee990913832a408d81c",
